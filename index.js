80ca271bf8b91830c2803574455f8537fea933f6
Leytox <iladevder@gmail.com>
2024-10-06 20:10:55 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB || "")
+  .then(() => {
+    console.log(
+      `Successfully connected to MongoDB at ${mongoose.connection.host}`,
+    );
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server is running on port ${process.env.PORT || 5000}`),
+    );
+  })
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
