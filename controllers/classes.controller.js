6bf81300293c29fae656927750c5eebffa39b5df
Ilya Devder <iladevder@gmail.com>
2024-11-11 13:31:27 +0200

    changed name of the model and controller to be exact as in the game

--- controllers/classes.controller.js
+++ controllers/classes.controller.js
@@ -1,6 +1,6 @@
-import Team from "../models/team.model.js";
+import Class from "../models/class.model.js";
 
-const createTeam = async (req, res) => {
+const createClass = async (req, res) => {
   try {
     const { name, description, faction, image } = req.body;
 
@@ -9,48 +9,48 @@ const createTeam = async (req, res) => {
       return res.status(400).json({ message: "Missing required fields" });
     }
 
-    const newTeam = new Team({
+    const newClass = new Class({
       name,
       description,
       faction,
       image,
     });
 
-    const savedTeam = await newTeam.save();
-    return res.status(201).json(savedTeam);
+    const savedClass = await newClass.save();
+    return res.status(201).json(savedClass);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-const getAllTeams = async (_req, res) => {
+const getAllClasses = async (_req, res) => {
   try {
-    const maps = await Team.find(undefined, undefined, undefined);
+    const maps = await Class.find(undefined, undefined, undefined);
     return res.status(200).json(maps);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-const getTeamByName = async (req, res) => {
+const getClassByName = async (req, res) => {
   const { name } = req.params;
   try {
-    const map = await Team.findOne(name, undefined, undefined);
-    if (!map) return res.status(404).json({ message: "Team not found" });
+    const map = await Class.findOne(name, undefined, undefined);
+    if (!map) return res.status(404).json({ message: "Class not found" });
     return res.status(200).json(map);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-const getTeamById = async (req, res) => {
+const getClassById = async (req, res) => {
   try {
-    const map = await Team.findById(req.params.id, undefined, undefined);
-    if (!map) return res.status(404).json({ message: "Team not found" });
+    const map = await Class.findById(req.params.id, undefined, undefined);
+    if (!map) return res.status(404).json({ message: "Class not found" });
     return res.status(200).json(map);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-export default { createTeam, getAllTeams, getTeamByName, getTeamById };
+export default { createClass, getAllClasses, getClassByName, getClassById };
--- routers/classes.router.js
+++ routers/classes.router.js
@@ -1,11 +1,11 @@
 import Router from "express";
-import teamsController from "../controllers/classes.controller.js";
+import classesController from "../controllers/classes.controller.js";
 
 const router = Router();
 
-router.post("/", teamsController.createClass);
-router.get("/", teamsController.getAllClasses);
-router.get("/:name", teamsController.getClassByName);
-router.get("/:id", teamsController.getClassById);
+router.post("/", classesController.createClass);
+router.get("/", classesController.getAllClasses);
+router.get("/:name", classesController.getClassByName);
+router.get("/:id", classesController.getClassById);
 
 export default router;
