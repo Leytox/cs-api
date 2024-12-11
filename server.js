c12a68ab481810365413b6764245fd42e3ac3983
Ilya Devder <iladevder@gmail.com>
2024-12-12 01:52:57 +0200

    changed folder name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import indexRouter from "./routers/index.router.js";
+import indexRouter from "./routes/index.router.js";
 
 dotenv.config();
 
@@ -12,15 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGODB || "")
-  .then(() => {
-    console.log(
-      `Successfully connected to MongoDB at ${mongoose.connection.host}`,
-    );
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server is running on port ${process.env.PORT || 5000}`),
-    );
-  })
-  .catch((error) => console.error("Error connecting to MongoDB:", error));
+    .connect(process.env.MONGODB || "")
+    .then(() => {
+        console.log(
+            `Successfully connected to MongoDB at ${mongoose.connection.host}`,
+        );
+        app.listen(process.env.PORT || 5000, () =>
+            console.log(`Server is running on port ${process.env.PORT || 5000}`),
+        );
+    })
+    .catch((error) => console.error("Error connecting to MongoDB:", error));
 
 app.use("/api/v1", indexRouter);
